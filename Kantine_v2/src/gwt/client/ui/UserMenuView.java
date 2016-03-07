f486d4bf8541e4aaaedff456e79812fdbc78e7bf
Aleksander R. Kristiansen <alek.rk@gmail.com>
2016-03-07 18:30:35 +0100

    css

--- Kantine_v2/src/gwt/client/ui/UserMenuView.java
+++ Kantine_v2/src/gwt/client/ui/UserMenuView.java
@@ -4,10 +4,15 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 
 public class UserMenuView extends Composite {
 
 	private static UserMenuViewUiBinder uiBinder = GWT.create(UserMenuViewUiBinder.class);
+	@UiField Button button;
 
 	interface UserMenuViewUiBinder extends UiBinder<Widget, UserMenuView> {
 	}
@@ -16,4 +21,7 @@ public class UserMenuView extends Composite {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
+	@UiHandler("button")
+	void onButtonClick(ClickEvent event) {
+	}
 }
