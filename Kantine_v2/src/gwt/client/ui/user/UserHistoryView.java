cb767d8f38b6d8c44821d06b23a4efe6b58c6202
Aleksander R. Kristiansen <alek.rk@gmail.com>
2016-04-11 18:46:52 +0200

    saldo

--- Kantine_v2/src/gwt/client/ui/user/UserHistoryView.java
+++ Kantine_v2/src/gwt/client/ui/user/UserHistoryView.java
@@ -9,6 +9,8 @@ import com.google.gwt.user.client.ui.Widget;
 import gwt.shared.ItemDTO;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 
 /**
  * View for showing users purchase history
@@ -35,8 +37,8 @@ public class UserHistoryView extends Composite {
 
 		// adjust column widths
 		historyTable.getFlexCellFormatter().setWidth(0, 0, "50px");
-		historyTable.getFlexCellFormatter().setWidth(0, 1, "25px");
-		historyTable.getFlexCellFormatter().setWidth(0, 2, "50px");
+		historyTable.getFlexCellFormatter().setWidth(0, 1, "50px");
+		historyTable.getFlexCellFormatter().setWidth(0, 2, "25px");
 		historyTable.getFlexCellFormatter().setWidth(0, 3, "50px");
 
 		// style table
@@ -45,9 +47,14 @@ public class UserHistoryView extends Composite {
 
 
 		for (int i = 0; i < pList.size(); i++) {
-			historyTable.setText(i + 1, 0, pList.get(i).getName());
-			historyTable.setText(i + 1, 1, "" +(-(pList.get(i).getPrice())));
-			historyTable.setText(i + 1, 2, "" + pList.get(i).getDate());
+			historyTable.setText(i + 1, 0, "" + pList.get(i).getDate());
+			historyTable.setText(i + 1, 1, pList.get(i).getName());
+			if (pList.get(i).getName().equals("Tilføjet til Saldo")){
+				historyTable.setText(i + 1, 2, "" +(pList.get(i).getPrice()));
+			}else{
+				historyTable.setText(i + 1, 2, "" +(-(pList.get(i).getPrice())));
+			}			
+			
 			historyTable.setText(i + 1, 3, "" + pList.get(i).getSaldo());
 		}
 	}
